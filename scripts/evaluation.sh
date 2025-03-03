263e4d474ffbce2ed1e0ff0c343bc17f010e6b85
leezekun <lizekunlee@gmail.com>
2025-03-02 22:23:12 -0500

    update

--- scripts/evaluation.sh
+++ scripts/evaluation.sh
@@ -1,3 +1,5 @@
+cd ..
+
 models=("gpt-4o" "claude-3-5-sonnet-20241022" "gemini-2.0-flash-001" "gpt-4o-mini" 
         "qwen2.5-72b-instruct" "gemini-2.0-flash-thinking-exp" "llama3.1-70b-instruct" 
         "deepseek-r1" "o1")
@@ -9,20 +11,13 @@ tool_call_modes=("fc" "react" "act-only")
 for domain in "${domains[@]}"; do
     for tool_list in "${tool_lists[@]}"; do
         for tool_call_mode in "${tool_call_modes[@]}"; do
+            for model in "${models[@]}"; do
             CUDA_VISIBLE_DEVICES=$devices python run_evaluation.py \
                         --domain $domain \
                         --assistant_model $model \
                         --tool_list $tool_list \
                         --tool_call_mode $tool_call_mode
+            done
         done
     done
 done
-
-
-# Adversarial user agent (gpt-4o)
-CUDA_VISIBLE_DEVICES=$devices python run_evaluation.py \
-            --domain healthcare \
-            --user_model gpt-4o \
-            --assistant_model $model \
-            --tool_list full \
-            --tool_call_mode fc
